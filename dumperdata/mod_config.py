f4ca799f37d89893e5a320873cc658310e1ed410
root <root@iZm5ehftd4ce4kf92zb5yiZ.(none)>
2018-08-07 18:26:21 +0800

    增加全量导入方法

---
+++ dumperdata/mod_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#encoding:utf-8
+#name:mod_config.py
+
+import ConfigParser
+import os
+
+#获取config配置文件
+def getConfig(section, key):
+    config = ConfigParser.ConfigParser()
+    path = os.path.split(os.path.realpath(__file__))[0] + '/db.conf'
+    config.read(path)
+    return config.get(section, key)
+
+#其中 os.path.split(os.path.realpath(__file__))[0] 得到的是当前文件模块的目录
